609cace26b1b31e754b8d05d8728710e3246a361
Alejandro Rodríguez Cañete <ale.rodca04@gmail.com>
2023-06-26 17:30:25 +0200

    making MVVC

---
+++ EmojiApp/UI/PrincipalViewController/PrincipalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PrincipalViewController.swift
+//  EmojiApp
+//
+//  Created by Alejandro Rodríguez Cañete on 26/6/23.
+//
+
+import UIKit
+
+class PrincipalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
